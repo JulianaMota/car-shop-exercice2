a242991a35c85b5d1883271db7e9f5cc30fc61b5
Juliana Mota <38047358+JulianaMota@users.noreply.github.com>
2018-11-02 12:14:44 +0100

    detail fetch solve

--- js/detail.js
+++ js/detail.js
@@ -8,23 +8,23 @@ function loadOneCar(carID){
 
 function showOneCar(data) {
     console.log(data);
-    //const copy = template.cloneNode(true);
-    //const bigImg = document.querySelector(".largeImg");
+    const copy = template.cloneNode(true);
+    const bigImg = document.querySelector(".largeImg");
 
 
-    //copy.querySelector("h2").textContent=data.title.rendered;
+    copy.querySelector("h2").textContent=data.title.rendered;
     //copy.querySelector(".year span").textContent=data.acf.year;
     //copy.querySelector(".color span").textContent=data.acf.color;
     //copy.querySelector(".horse-power span").textContent=data.acf.horse_power;
-    //copy.querySelector(".price span").textContent=car.data.price;
+    copy.querySelector(".price span").textContent=data.acf.price;
     //copy.querySelector(".engine span").textContent=data.acf.engine;
     //copy.querySelector(".body").innerHTML=data.content.rendered;
     //copy.querySelector(".video").innerHTML=data.acf.video;
 
-    //document.querySelector("main").appendChild(copy);
+    document.querySelector("main").appendChild(copy);
 
     myLoader.classList.add("hide");
 
 }
 
-showOneCar(carID);
+loadOneCar(carID);
